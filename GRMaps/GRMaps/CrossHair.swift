332b6636a149e7124aad004ca2fe62c456a2fc8f
Tyler Miller <tyemiller4@gmail.com>
2016-08-15 01:12:43 -0400

    add addAnotaion
    Bug with dismissing UIAlertController resetting the TintColor for the
    MapView item

--- GRMaps/GRMaps/CrossHair.swift
+++ GRMaps/GRMaps/CrossHair.swift
@@ -15,27 +15,35 @@ import UIKit
 
 public class CrossHair : NSObject {
     
-
+    
     // Drawing Methods
-
+    
     public class func drawCross() {
+        
         // Color Declarations
         let color = UIColor(red: 1.000, green: 0.991, blue: 0.991, alpha: 1.000)
         let color2 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.000)
         
-        
+
         // BeamOne Drawing
-        let beamOnePath = UIBezierPath(rect: CGRect(x: 9, y: 0, width: 7, height: 25))
+        let beamOnePath = UIBezierPath(rect: CGRect(x: 12, y: 1, width: 3, height: 25))
         color2.setFill()
         beamOnePath.fill()
         
         
         // BeamTwo Drawing
-        let beamTwoPath = UIBezierPath(rect: CGRect(x: 0, y: 9, width: 25, height: 7))
+        let beamTwoPath = UIBezierPath(rect: CGRect(x: 1, y: 12, width: 25, height: 3))
         color.setFill()
         beamTwoPath.fill()
+        
+        
+        // Oval Drawing
+        let ovalPath = UIBezierPath(ovalInRect: CGRect(x: 1, y: 1, width: 25, height: 25))
+        UIColor.whiteColor().setStroke()
+        ovalPath.lineWidth = 2
+        ovalPath.stroke()
     }
     
-   
-  
+    
+    
 }
--- GRMaps/GRMaps/ViewController.swift
+++ GRMaps/GRMaps/ViewController.swift
@@ -11,11 +11,11 @@ import Mapbox
 
 
 class ViewController: UIViewController, MGLMapViewDelegate {
-
-    @IBOutlet var mapView: MGLMapView!
-    var point = MGLPointAnnotation()
     
-   
+    @IBOutlet var mapView: MGLMapView!
+    @IBOutlet weak var panBtn: MARoundButton!
+    @IBOutlet weak var addAnotationBtn: MARoundButton!
+    
     
     // MARK: View
     override func viewDidLoad() {
@@ -25,26 +25,19 @@ class ViewController: UIViewController, MGLMapViewDelegate {
         self.mapView.showsUserLocation = true
         self.mapView.userTrackingMode = .Follow
         
-        
-
-        
-        
-        
-        // Chnage Information tint 
-        mapView.attributionButton.tintColor = UIColor.grayColor()
-    
-        
+        setTintColors()
         
     }
     
-
-
-    func mapView(mapView: MGLMapView, didUpdateUserLocation userLocation: MGLUserLocation?) {
-//        point.coordinate =  self.mapView.convertPoint(self.mapView.center, toCoordinateFromView: self.mapView)
-//        self.mapView.addAnnotation(point)
+    // MARK: Tint Colors
+    func setTintColors() {
+        // Change Information, UIButtons tint
         
+        self.mapView.attributionButton.tintColor = UIColor.grayColor()
+        self.addAnotationBtn.tintColor = UIColor.whiteColor()
+        self.panBtn.tintColor = UIColor.lightGrayColor()
     }
-   
+    
     // MARK: IBActions
     
     @IBAction func panToUser() {
@@ -58,10 +51,54 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     
     @IBAction func placeMarker() {
         
-        let pointOfInterest = MGLPointAnnotation()
-        pointOfInterest.coordinate = self.mapView.convertPoint(self.mapView.center, toCoordinateFromView: self.mapView)
-        pointOfInterest.title = "Test"
-        mapView.addAnnotation(pointOfInterest)
+        
+        
+        // Text Input Alert
+        let alert = UIAlertController(title: "Location Name", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler({ (textField) in
+            textField.placeholder = "Name"
+            textField.addTarget(self, action: #selector(self.textChanged), forControlEvents: .EditingChanged)
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+        
+        let doneAction = UIAlertAction(title: "Done", style: .Default) { (_) in
+            
+            
+            // Add Anotation for user selected location
+            let pointOfInterest = MGLPointAnnotation()
+            pointOfInterest.coordinate = self.mapView.convertPoint(self.mapView.center, toCoordinateFromView: self.mapView)
+            pointOfInterest.title = alert.textFields![0].text
+            self.mapView.addAnnotation(pointOfInterest)
+            
+            
+            
+        }
+        
+        
+        alert.addAction(cancelAction)
+        alert.addAction(doneAction)
+        alert.actions[1].enabled = false
+        alert.view.setNeedsLayout()
+        self.presentViewController(alert, animated: true, completion: nil)
+        
     }
+    
+  
+    func textChanged(sender:AnyObject) {
+        
+        let tField = sender as! UITextField
+        
+        // Enable Done Button if text has been entered
+        var responder : UIResponder! = tField
+        
+        while !(responder is UIAlertController) { responder = responder.nextResponder() }
+        
+        let alert = responder as! UIAlertController
+        alert.actions[1].enabled = (tField.text != "")
+        
+    }
+    
+  
 }
 
